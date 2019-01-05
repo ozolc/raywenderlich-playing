e2c2bb8578bce2f727cc5c415f592208041aa40c
Maksim Nosov <ozolc@yandex.ru>
2019-01-05 17:24:34 +0300

    Mastering the Matrix - added marker on arc

--- Fio/Fio/CounterView.swift
+++ Fio/Fio/CounterView.swift
@@ -85,6 +85,43 @@ import UIKit
         outlineColor.setStroke()
         outlinePath.lineWidth = Constants.lineWidth
         outlinePath.stroke()
+        
+        // Counter View markers
+        let context = UIGraphicsGetCurrentContext()!
+        
+        // 1 - save original state
+        context.saveGState()
+        outlineColor.setFill()
+        
+        let markerWidth: CGFloat = 5.0
+        let markerSize: CGFloat = 10.0
+        
+        // 2 - the marker rectangle positioned at the top left
+        let markerPath = UIBezierPath(rect: CGRect(x: -markerWidth / 2, y: 0, width: markerWidth, height: markerSize))
+        
+        // 3 - move top left of context to the previous center position
+        context.translateBy(x: rect.width / 2, y: rect.height / 2)
+        
+        for i in 1...Constants.numberOfGlasses {
+            // 4 - save the centered context
+            context.saveGState()
+            // 5 - calculate the rotation angel
+            let angle = arcLengthPerGlass * CGFloat(i) + startAngle - .pi / 2
+            // rotate and translate
+            context.rotate(by: angle)
+            context.translateBy(x: 0, y: rect.height / 2 - markerSize)
+            
+            // 6 - fill the marker rectangle
+            markerPath.fill()
+            
+            // 7 - restore the centered context for the next rotate
+            context.restoreGState()
+        }
+        
+        // 8 - restore the original state in case of more painting
+        context.restoreGState()
     }
+    
+    
     
 }
