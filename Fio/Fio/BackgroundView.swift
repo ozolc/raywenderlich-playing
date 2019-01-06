99382f3b12cecc48a545de2671c35b822d3e4a1a
Maksim Nosov <ozolc@yandex.ru>
2019-01-06 13:05:04 +0300

    Added Background Repeating Pattern

---
+++ Fio/Fio/BackgroundView.swift
@@ -0,0 +1,69 @@
+//
+//  BackgroundView.swift
+//  Fio
+//
+//  Created by Maksim Nosov on 06/01/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class BackgroundView: UIView {
+
+    // 1
+    @IBInspectable var lightColor: UIColor = UIColor.orange
+    @IBInspectable var darkColor: UIColor = UIColor.yellow
+    @IBInspectable var patternSize: CGFloat = 30
+    
+    override func draw(_ rect: CGRect) {
+        // 2
+        let context = UIGraphicsGetCurrentContext()!
+        
+        // 3
+        context.setFillColor(darkColor.cgColor)
+        
+        // 4
+        context.fill(rect)
+        
+        let drawSize = CGSize(width: patternSize, height: patternSize)
+        
+        UIGraphicsBeginImageContextWithOptions(drawSize, true, 0.0)
+        let drawingContext = UIGraphicsGetCurrentContext()!
+        
+        // set the fill color for the new context
+        darkColor.setFill()
+        drawingContext.fill(CGRect(x: 0, y: 0, width: drawSize.width, height: drawSize.height))
+        
+        let trianglePath = UIBezierPath()
+        // 1
+        trianglePath.move(to: CGPoint(x: drawSize.width / 2, y: 0))
+        // 2
+        trianglePath.addLine(to: CGPoint(x: 0, y: drawSize.height / 2))
+        // 3
+        trianglePath.addLine(to: CGPoint(x: drawSize.width, y: drawSize.height / 2))
+        
+        // 4
+        trianglePath.move(to: CGPoint(x: 0, y: drawSize.height / 2))
+        // 5
+        trianglePath.addLine(to: CGPoint(x: drawSize.width / 2, y: drawSize.height))
+        // 6
+        trianglePath.addLine(to: CGPoint(x: 0, y: drawSize.height))
+        
+        // 7
+        trianglePath.move(to: CGPoint(x: drawSize.width, y: drawSize.height / 2))
+        // 8
+        trianglePath.addLine(to: CGPoint(x: drawSize.width / 2, y: drawSize.height))
+        // 9
+        trianglePath.addLine(to: CGPoint(x: drawSize.width, y: drawSize.height))
+        
+        
+        lightColor.setFill()
+        trianglePath.fill()
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        UIColor(patternImage: image).setFill()
+        context.fill(rect)
+    }
+}
