e729069872655944b6dc6bb400953a763c0533dc
Maksim Nosov <ozolc@yandex.ru>
2019-01-07 11:53:51 +0300

    Images vs. CoreGraphics

--- CustomSliderExample/CustomSliderExample/RangeSlider.swift
+++ CustomSliderExample/CustomSliderExample/RangeSlider.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class RangeSlider: UIControl {
 
@@ -14,5 +15,63 @@ class RangeSlider: UIControl {
     var maximumValue = 1.0
     var lowerValue = 0.2
     var upperValue = 0.8
-
+    
+    let trackLayer = CALayer()
+    let lowerThumbLayer = CALayer()
+    let upperThumbLayer = CALayer()
+    
+    var thumbWidth: CGFloat {
+        return CGFloat(bounds.height)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        trackLayer.backgroundColor = UIColor.blue.cgColor
+        layer.addSublayer(trackLayer)
+        
+        lowerThumbLayer.backgroundColor = UIColor.green.cgColor
+        layer.addSublayer(lowerThumbLayer)
+        
+        upperThumbLayer.backgroundColor = UIColor.green.cgColor
+        layer.addSublayer(upperThumbLayer)
+        
+        updateLayerFrames()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateLayerFrames() {
+        trackLayer.frame = bounds.insetBy(dx: 0.0, dy: bounds.height / 3)
+        trackLayer.setNeedsDisplay()
+        
+        let lowerThumbCenter = CGFloat(positionForValue(value: lowerValue))
+        
+        lowerThumbLayer.frame = CGRect(x: lowerThumbCenter - thumbWidth / 2.0,
+                                       y: 0.0,
+                                       width: thumbWidth,
+                                       height: thumbWidth)
+        lowerThumbLayer.setNeedsDisplay()
+        
+        let upperThumbCenter = CGFloat(positionForValue(value: upperValue))
+        upperThumbLayer.frame = CGRect(x: upperThumbCenter - thumbWidth / 2.0,
+                                       y: 0.0,
+                                       width: thumbWidth,
+                                       height: thumbWidth)
+        upperThumbLayer.setNeedsDisplay()
+    }
+    
+    func positionForValue(value: Double) -> Double {
+        return Double(bounds.width - thumbWidth) * (value - minimumValue) /
+        (maximumValue - minimumValue) + Double(thumbWidth / 2.0)
+    }
+    
+    override var frame: CGRect {
+        didSet {
+            updateLayerFrames()
+        }
+    }
+    
 }
