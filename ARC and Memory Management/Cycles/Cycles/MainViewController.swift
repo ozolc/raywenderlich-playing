e7605ce4c97303abac889628d1504231b47d3d26
Maksim Nosov <ozolc@yandex.ru>
2019-03-25 17:17:25 +0300

    Is That in Scope?

--- ARC and Memory Management/Cycles/Cycles/MainViewController.swift
+++ ARC and Memory Management/Cycles/Cycles/MainViewController.swift
@@ -29,7 +29,28 @@
 import UIKit
 
 class MainViewController: UIViewController {
+//  let user = User(name: "John")
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    runScenario()
+  }
+  
+  func runScenario() {
+    let user = User(name: "John")
+  }
+  
+}
+
+class User {
+  let name: String
+  
+  init(name: String) {
+    self.name = name
+    print("User \(name) was initialized.")
+  }
+  
+  deinit {
+    print("Deallocating user named: \(name)")
   }
 }
