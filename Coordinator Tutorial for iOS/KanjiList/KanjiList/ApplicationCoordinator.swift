8fbbb767611178cd1b7e1106b556a443dc295d18
Maksim Nosov <ozolc@yandex.ru>
2019-10-06 10:41:57 +0300

    Application Coordinator

--- Coordinator Tutorial for iOS/KanjiList/KanjiList/ApplicationCoordinator.swift
+++ Coordinator Tutorial for iOS/KanjiList/KanjiList/ApplicationCoordinator.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2018 Razeware LLC
-///
+/// Copyright (c) 2019 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,22 +28,26 @@
 
 import UIKit
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  
-    var window: UIWindow?
-    private var applicationCoordinator: ApplicationCoordinator?
-  
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+class ApplicationCoordinator: Coordinator {
+    let kanjiStorage: KanjiStorage
+    let window: UIWindow
+    let rootViewController: UINavigationController
     
-    let window = UIWindow(frame: UIScreen.main.bounds)
-    let applicationCoordinator = ApplicationCoordinator(window: window)
+    init(window: UIWindow) {
+        self.window = window
+        kanjiStorage = KanjiStorage()
+        rootViewController = UINavigationController()
+        rootViewController.navigationBar.prefersLargeTitles = true
+        
+        let emptyViewController = UIViewController()
+        emptyViewController.view.backgroundColor = .cyan
+        emptyViewController.title = "Title"
+        rootViewController.pushViewController(emptyViewController, animated: false)
+        
+    }
     
-    self.window = window
-    self.applicationCoordinator = applicationCoordinator
-    
-    applicationCoordinator.start()
-    return true
-  }
+    func start() {
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+    }
 }
-
